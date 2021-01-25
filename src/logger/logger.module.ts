5ae88cae4824319b7043e144f735450fdb6c8a40
Chris Meller <chris@doesnthaveone.com>
2021-01-25 18:22:54 +0200

    Fix DI for Logger Module.

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from 'src/config/config.module';
 import { LoggerService } from './logger.service';
 
 @Module({
-  providers: [LoggerService]
+  providers: [LoggerService],
+  imports: [ConfigModule]
 })
 export class LoggerModule {}
