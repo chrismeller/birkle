affd687e7b3bb37225b3e3b91b7a289da132d2d5
Chris Meller <chris@doesnthaveone.com>
2021-01-25 14:38:36 +0200

    Add config service with dotenv dependency.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [ConfigModule],
 })
 export class AppModule {}
