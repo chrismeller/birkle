818d0f9c0c9b249df6f5c537b2d09efb6d8aa1b8
Chris Meller <chris@doesnthaveone.com>
2021-01-25 18:24:48 +0200

    Bootstrap a User module that we'll start adding bits to.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { LoggerModule } from './logger/logger.module';
+import { UsersModule } from './users/users.module';
 import { DbModule } from './db/db.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ConfigModule, LoggerModule, DbModule],
+  imports: [ConfigModule, LoggerModule, UsersModule, DbModule],
 })
 export class AppModule {}
