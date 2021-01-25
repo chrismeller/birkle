118724682b869224d42a74fd6d2f721857315ef6
Chris Meller <chris@doesnthaveone.com>
2021-01-25 18:13:27 +0200

    Fix the config module to specify the file path for the ConfigService constructor.

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from './config.service';
+import * as path from 'path';
 
 @Module({
-  providers: [ConfigService]
+  providers: [
+    {
+      provide: ConfigService,
+      useValue: new ConfigService(`${process.env.NODE_ENV || 'development'}.env`),
+    },
+  ],
+  exports: [ConfigService],
 })
 export class ConfigModule {}
