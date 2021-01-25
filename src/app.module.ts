352fe67800c6766017237657b6dccda1cd9fdc0f
Chris Meller <chris@doesnthaveone.com>
2021-01-25 15:13:04 +0200

    Add logger service that wraps Winston.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ConfigModule],
+  imports: [ConfigModule, LoggerModule],
 })
 export class AppModule {}
