044da56d23dd3f4b2a50b38685398990d194c51d
Chris Meller <chris@doesnthaveone.com>
2021-01-26 18:19:30 +0200

    We also need to include the salt we generated for the user's password along with the rest of the user info so we can compare against it later.

---
+++ prisma/migrations/20210126150529_add_password_salt/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - Added the required column `PasswordSalt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "Id" TEXT NOT NULL PRIMARY KEY,
+    "Email" TEXT NOT NULL,
+    "FirstName" TEXT NOT NULL,
+    "Surname" TEXT NOT NULL,
+    "PasswordHash" TEXT NOT NULL,
+    "PasswordSalt" TEXT NOT NULL,
+    "EmailVerifiedAt" DATETIME,
+    "VerificationToken" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("Id", "Email", "PasswordHash", "FirstName", "Surname", "EmailVerifiedAt", "VerificationToken") SELECT "Id", "Email", "PasswordHash", "FirstName", "Surname", "EmailVerifiedAt", "VerificationToken" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User.Email_unique" ON "User"("Email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
