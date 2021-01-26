61f7966c31d8882e203a6c728bddc941e9971f84
Chris Meller <chris@doesnthaveone.com>
2021-01-26 14:36:52 +0200

    Add some additional fields to the User db model that we'll need for actual operation of the auth system.

---
+++ prisma/migrations/20210126122910_add_user_password/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `PasswordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "Id" TEXT NOT NULL PRIMARY KEY,
+    "Email" TEXT NOT NULL,
+    "PasswordHash" TEXT NOT NULL,
+    "FirstName" TEXT NOT NULL,
+    "Surname" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("Id", "Email", "FirstName", "Surname") SELECT "Id", "Email", "FirstName", "Surname" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User.Email_unique" ON "User"("Email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
