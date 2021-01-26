b0852dba30bfa1e83924d91cc92e9aa2f7774a10
Chris Meller <chris@doesnthaveone.com>
2021-01-26 14:10:46 +0200

    Abandon our custom logger that wraps Winston, it was less robust than the built-in Nest logger for our current console logging.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
-import { LoggerModule } from './logger/logger.module';
 import { UsersModule } from './users/users.module';
 import { DbModule } from './db/db.module';
 
@@ -10,5 +9,6 @@ import { DbModule } from './db/db.module';
   controllers: [AppController],
   providers: [AppService],
   imports: [ConfigModule, LoggerModule, UsersModule, DbModule],
+  imports: [ConfigModule, DbModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useLogger(await app.resolve('LoggerService'));
-
   await app.listen(30000);
 }
 bootstrap();
